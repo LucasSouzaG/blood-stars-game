4638300ded06a0cc681502c53f0ad4311417e154
Lucas Souza <lucas.souza@leoo.com.br>
2023-11-27 19:16:32 -0300

    join main_menu with game

--- main.py
+++ main.py
@@ -1,12 +1,78 @@
 from screens import Menu
-fase = 1
+from screens.main_menu import Main_menu
+from screens.learning_game import Learning_Game
+from utils import calculate_screen_center, button_learning_game, button_return
+from settings import WINDOW_RESOLUTION
+import pygame
 
-if fase == 3:
-    from screens.planet_mars import Marte
+run = True
 
-if fase == 2:
-    from screens.galaxy import Galaxy
+menu = Main_menu()
+menu_display = menu.display()
+menu_open = True
 
-if fase == 1:
-    if __name__ == "__main__":
-        Menu.main()
+learning_game = Learning_Game()
+learning_game_open = True
+
+while run:
+    for event in pygame.event.get():
+
+        if menu_open:
+            menu_display = menu.display_refresh()
+            menu_display.blit(menu.background(), (0,0))
+            menu_display.blit(menu.title(), calculate_screen_center(WINDOW_RESOLUTION, menu.title()))
+            menu_display.blit(button_learning_game(), (10,10))
+            learning_game_open = True
+            menu_open = False
+            
+        if event.type == pygame.QUIT:
+            run = False
+
+        elif event.type == pygame.KEYDOWN:  # Verifica se uma tecla foi pressionada
+            if event.key == pygame.K_ESCAPE:  # Verifica se a tecla pressionada foi o "ESC"
+                run = False
+            elif learning_game_open == True:
+                Menu.main()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:  # Clique do botão esquerdo do mouse
+                        if x > 9 and x < 46 and y > 9 and y < 46:  # Defina as coordenadas do botão de transição
+                            menu_open = True
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN: # Verifica o clique do mouse
+            if event.button == 1:  # Clique do botão esquerdo do mouse
+                x, y = event.pos
+                print(f'Posicao do clique {x},{y}')  # Posição do clique
+
+                if x > 9 and x < 46 and y > 9 and y < 46:  # Defina as coordenadas do botão de transição
+                    if learning_game_open:
+                        learning_game_display = learning_game.display()
+                        learning_game_display.blit(learning_game.background(), (0,0)) # Imagem de fundo
+
+                        learning_game_display.blit(button_return(), (10,10)) # Imagem/botão de retorno
+
+                        learning_game_display.blit(learning_game.ship(360), (120,300)) # Imagem nave
+                        learning_game_display.blit(learning_game.arrow(90), (166,305)) # Imagem seta
+                        learning_game_display.blit(learning_game.arrow(90), (210,305)) # Imagem seta
+                        learning_game_display.blit(learning_game.button(90), (160,350)) # imagem botão
+                        
+                        learning_game_display.blit(learning_game.ship(360), (360,215)) # Imagem nave
+                        learning_game_display.blit(learning_game.arrow(360), (365,305)) # Imagem seta
+                        learning_game_display.blit(learning_game.arrow(360), (365,270)) # Imagem seta
+                        learning_game_display.blit(learning_game.button(360), (360,350)) # imagem botão
+
+                        learning_game_display.blit(learning_game.ship(360), (515,300)) # Imagem nave
+                        learning_game_display.blit(learning_game.arrow(180), (520,215)) # Imagem seta
+                        learning_game_display.blit(learning_game.arrow(180), (520,260)) # Imagem seta
+                        learning_game_display.blit(learning_game.button(180), (515,350)) # imagem botão
+
+                        learning_game_display.blit(learning_game.ship(360), (780,300)) # Imagem nave
+                        learning_game_display.blit(learning_game.arrow(270), (745,305)) # Imagem seta
+                        learning_game_display.blit(learning_game.arrow(270), (700,305)) # Imagem seta
+                        learning_game_display.blit(learning_game.button(270), (740,350))
+
+                        learning_game_open = False
+                    else:
+                        menu_open = True
+
+    pygame.display.update()
--- screens/menu.py
+++ screens/menu.py
@@ -1,29 +1,82 @@
 import pygame
-from settings import WINDOW_RESOLUTION
-from pygame.sprite import Sprite
+import sys
+import os
 
-class Menu(Sprite):
-    def __init__(self):
-        self.window_name = 'Menu Principal'
+nave_selecionada = ""
 
-    def display(self):
-        pygame.init()
-        display = pygame.display.set_mode(WINDOW_RESOLUTION)
-        pygame.display.set_caption(self.window_name)
-        return display
+def draw_menu(screen, white, selected_ship, ship_images):
+    screen.fill(white)
 
-    def display_refresh(self):
-        display = pygame.display.set_mode(WINDOW_RESOLUTION)
-        pygame.display.set_caption(self.window_name)
-        return display
-    
-    def background(self):
-        image_background = pygame.image.load('screens\images\menu_&_learning_game_background.jpeg').convert_alpha()
-        image_background = pygame.transform.scale(image_background, WINDOW_RESOLUTION)
-        return image_background
-    
-    def title(self):
-        # Link para fonte https://fontmeme.com/pixel-fonts/
-        image_title = pygame.image.load('screens\images\menu_title.png').convert_alpha()
-        image_title = pygame.transform.scale(image_title, (624,30))
-        return image_title
+    for i, image in enumerate(ship_images):
+        x = (i + 1) * (screen_width // 5) - image.get_width() // 2
+        y = screen_height // 2 - image.get_height() // 2
+        screen.blit(image, (x, y))
+
+        if selected_ship == i:
+            pygame.draw.rect(screen, (255, 0, 0), (x - 5, y - 5, image.get_width() + 10, image.get_height() + 10), 2)
+
+    font = pygame.font.Font(None, 36)
+    start_text = font.render("Start", True, (0, 0, 0))
+    global start_rect
+    start_rect = start_text.get_rect(center=(screen_width // 2, screen_height - 50))
+    pygame.draw.rect(screen, (0, 255, 0),
+                     (start_rect.x - 5, start_rect.y - 5, start_rect.width + 10, start_rect.height + 10), 2)
+    screen.blit(start_text, start_rect)
+
+    pygame.display.flip()
+
+def load_ship_images():
+    ship_paths = [
+        "screens/Ships/ship_0000.png",
+        "screens/Ships/ship_0001.png",
+        "screens/Ships/ship_0002.png",
+        "screens/Ships/ship_0003.png"
+    ]
+    ship_images = [pygame.image.load(path) for path in ship_paths]
+    return ship_images, ship_paths
+
+def main():
+    global nave_selecionada, selected_ship, screen_width, screen_height
+
+    pygame.init()
+
+    screen_width = 1000
+    screen_height = 600
+    selected_ship = None
+
+    white = (200, 255, 255)
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Menu de Seleção de Nave")
+
+    ship_images, ship_paths = load_ship_images()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rect.collidepoint(event.pos):
+                    if selected_ship is not None:
+                        print("Você escolheu a nave", selected_ship + 1)
+                        print("Caminho da nave selecionada:", nave_selecionada)
+                        # Restante do código para iniciar o jogo ou realizar outras ações
+                        from screens.galaxy import Galaxy
+                        running = False
+                else:
+                    for i, image in enumerate(ship_images):
+                        x = (i + 1) * (screen_width // 5) - image.get_width() // 2
+                        y = screen_height // 2 - image.get_height() // 2
+                        rect = pygame.Rect(x - 5, y - 5, image.get_width() + 10, image.get_height() + 10)
+                        if rect.collidepoint(event.pos):
+                            nave_selecionada = ship_paths[i]
+                            selected_ship = i
+
+        draw_menu(screen, white, selected_ship, ship_images)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
